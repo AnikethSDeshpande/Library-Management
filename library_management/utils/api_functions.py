f8020e2d2a8e196e5f619c2284c882ae90043732
AnikethSDeshpande <anikethsdeshpande@gmail.com>
2020-05-06 02:07:21 +0530

    es working well

--- library_management/utils/api_functions.py
+++ library_management/utils/api_functions.py
@@ -13,6 +13,7 @@ def get_key_name(fdt, fdn):
 	frappe.msgprint(key_name)
 	return key_name
 
+
 @frappe.whitelist()
 def get_details(msg):
     frappe.msgprint("get_details says, {0}".format(msg))
@@ -56,4 +57,62 @@ def get_articles(search_string):
     
 
     return articles
+
+
+
+@frappe.whitelist()
+def get_items(search_string, shop):
+    es = Elasticsearch()
+
+    body = {
+        "size": 20, 
+        "query": {
+            "bool": {
+            "must": [
+                {
+                    "match": {
+                        "item_name": {"query": search_string, "analyzer": "standard"}
+                    }
+                }
+            ],
+            
+            "should": [
+                {
+                    "term": { "shop_name": shop }
+                },
+                {
+                    "term": { "shop_name": "all_catalog_items" }
+                }
+            ],
+            "minimum_should_match": 1 
+            }
+        },
+        "sort": [
+            {
+            "als_score": {
+                "order": "desc"
+            }
+            }
+        ]
+    }
+    results = es.search(index='catalog', body=body, size=15)
+
+    items = []
+
+    for result in results['hits']['hits']:
+        item_name = result['_source']['item_name']
+        # item_code = result['_source']['item_code']
+        # item_sub_category = result['_source']['item_sub_category']
+        # item_group = result['_source']['item_group']
+
+        items.append(item_name)
+    
+    print(items)
+    
+    return items
+
+
+@frappe.whitelist()
+def display_message():
+    frappe.msgprint('Hey, you saved somehting!')
 
